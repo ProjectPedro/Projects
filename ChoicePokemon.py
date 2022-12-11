8ad4816632dab6a1ae1bbc70523910b3bceba7eb
ProjectPedro <120329308+ProjectPedro@users.noreply.github.com>
2022-12-11 14:47:19 -0300

    Update ChoicePokemon.py

--- ChoicePokemon.py
+++ ChoicePokemon.py
@@ -1,18 +1,22 @@
 # Projects
-Projeto De Estudos PY
+#Projeto De Estudos PY
 #POKEMON CHOICE 
 #LEARNING ABOUT PY
-
-choice = input( 'Hi, im From Pokeland And I will help you with you first choice "Charmander", "Squirtle", "Bulbasaur"\nChoice :')
+#PROJETO DE ESTUDOS QUE EU FIZ TOTALMENTE EM INGLES
+#choicepokemon.py
+choice = input('Hi, im From Pokeland And I will help you with you first choice "Charmander", "Squirtle", "Bulbasaur"\nChoice :')
 
 if choice == "Charmander":
-    print('OOh thats Nice,Your Choice a fire pokemon,he its strong against leaf pokemons and weak against water pokemons')
+    print('\nOh thats Nice,Your Choice a fire pokemon,he its strong against leaf pokemons and weak against water pokemons')
 
 elif choice == "Squirtle":
-    print('Squirtle,A great Acqua Pokemon,he its strong against fire pokemons ,but weak against leaf pokemons')
+    print('\nSquirtle,A great Acqua Pokemon,he its strong against fire pokemons ,but weak against leaf pokemons')
 
 elif choice == "Bulbasaur":
-    print('EVOLVES QUICKLY,He its strong against acqua pokemons,but weak against fire pokemons')
+    print('\nEVOLVES QUICKLY,He its strong against acqua pokemons,but weak against fire pokemons')
 
 else:
-    print('You Need to choice One of the three options.')
+    print('\nYou Need to choice One of the three options.')
+
+print('\nDeveloped By ProjectPedro')
+
